ccd7c80becd799049657d42a84878661e7ef20bd
lilgmills <laura.a.etale@gmail.com>
2024-12-05 23:57:13 -0600

    general functionality works but buggy

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -4,76 +4,207 @@ const todoMain = document.getElementById("todo-main-section");
 const inputBox = document.getElementById("write-todo");
 const submitButton =  document.getElementById("create-todo");
 
-const currPagePointer = 0;
-const currPageArr = [];
-const memObj = {
-    0: {
-        title: "Page1",
-        items:currPageArr
-    }
-};
+let currPagePointer = 0;
+let currPageArr = [];
+let memObj = [];
+
+function resetMemObj() {
+    memObj = [];
+    memObj.unshift({
+        title: "New Page",
+        items:[],
+        status:[],
+    });
+}
+
+resetMemObj();
+//memObj will just be an array of objects, which nests another array in the "items" field
 
 //to read/update todo items on a page:
 // memObj[<currPagePointer>].items
 
-
+let ptTo = (obj, idx) => {
+    return obj.length - idx - 1
+}
 
 function putTodoInCache(todoText) {
-    //insert todo at beginning of page
     currPageArr.unshift(todoText);
-    //this is really bad because we are re-writing the entire page content into an array each time we add a todo
-    //much better would be to update only a linked list with a new starting node, as the value of the "items" key
-    //the key would simply point to the first item in the list, which points to the next item, and so on
-    //the last item would not point to anything
-    memObj[currPagePointer].items = currPageArr;
+    memObj[currPagePointer].status.unshift(false);
+    
+    memObj[currPagePointer].items.unshift(todoText);
 
 }
 
+function putNewPageInCache(title) {
+    memObj.unshift({title:title, items:[], status:[]})
+    currPageArr=[];
+    currPagePointer=0;
+}
+
+function loadTodoOnCurrentPage(newText, status, index) {
+    const newDiv = document.createElement('div');
+    const newCheckbox = document.createElement('input');
+    const txtContainer = document.createElement('div');
+
+    newCheckbox.setAttribute('type','checkbox');
+    newCheckbox.setAttribute('class','todo-checkmark');
+
+    const newP = document.createElement('p');
+    newP.textContent = newText;
+    txtContainer.appendChild(newP);
+    txtContainer.classList.add('txt-container');
+    newP.setAttribute("id",String(index));
+
+    newDiv.setAttribute('class', 'checklist-item')
+    if(status) {
+        newDiv.classList.add('checked');
+    }
+    else {
+        newDiv.classList.add('unchecked');
+
+    }
+    
+    newDiv.appendChild(newCheckbox);
+    newDiv.appendChild(txtContainer);
+
+    todoMain.appendChild(newDiv);
+    //createIndexedEventListenerForChecks(newCheckbox, newP, newDiv, index);
+
+        
+}
+
 function readAndLoadPageFromCache(targetPointer) {
-    const newCurrentPage = memObj[targetPointer]
-    const pageTitle = newCurrentPage.title;
-    const pageItems = newCurrentPage.items;
+    
+    currPagePointer = targetPointer;
+
+    const pageTitle = memObj[targetPointer].title;
+    const pageItems = memObj[targetPointer].items;
+    const pageStatus = memObj[targetPointer].status;
+    currPageArr = pageItems;
 
     todoMain.replaceChildren();
 
-    pageItems.forEach(item, ()=>{createTodoOnCurrentPage(item)})
+    const RANGE = pageItems.length;
+
+    for(i=0;i<RANGE;i++) {
+        loadTodoOnCurrentPage(pageItems[i], pageStatus[i], i);
+        
+    }
+    const todoElements = todoMain.children;
+
+    Array.from(todoElements).forEach((currDiv, index) => {
+        if(pageStatus[index]) {
+            currDiv.classList.add('checked');
+            currDiv.children[1].children[0].classList.add('checked-item');
+        }
+        else {
+            currDiv.classList.add('unchecked');
+            currDiv.children[1].children[0].classList.add('unchecked-item');
+        }
+    })
+
+    reindexTodoIds();
 
     const UITitle = document.getElementById("UI-title");
     UITitle.innerText = pageTitle;
 
 }
 
-function createEventListenerForChecks(checkbox, para) {
+function createNewTodoPage() {
+    //0 will always be at the top and they will index normally down the page
+    
+    const defaultTitle = "New Page"
+    const newSidebarContent = document.createElement('div');
+    const newP = document.createElement('p');
+
+    newSidebarContent.classList.add("sidebar-content");
+
+    newP.textContent = defaultTitle;
+    newSidebarContent.appendChild(newP);
+
+    const menu = document.getElementById("side-menu-container");
+    //insert the new page title at the beginning of the list
+    menu.prepend(newSidebarContent);
+
+    const sideBarContent = menu.children;
+    // .children returns HTMLCollection
+
+    putNewPageInCache(defaultTitle);
+
+    Array.from(sideBarContent).forEach((item, index) => {
+        item.setAttribute("id", `sidebar-page-${index}`)
+        
+        function createInputHandlerById(index) {
+            return function (event) {
+                readAndLoadPageFromCache(index);
+            }
+        }
+
+        item.onclick = createInputHandlerById(index);
+
+    })
+
+    
+
+    //need to update event listeners when this happens? or just create one new event listener
+
+}
+
+document.getElementById('create-new-page').onclick = () => {
+    createNewTodoPage();
+    readAndLoadPageFromCache(0);
+}
+
+function createEventListenerForChecks(checkbox, para, containDiv) {
   checkbox.addEventListener('click', () => {
-    if (checkbox.checked) {
-      para.classList.add('checked-item')
-      
+    if (containDiv.classList.contains('unchecked')) {
+      para.classList.add('checked-item');
+      containDiv.classList.remove('checked');
+      containDiv.classList.add('unchecked');
+
     } else {
-      para.classList.remove('checked-item')
+      para.classList.remove('checked-item');
+      memObj[currPagePointer].status.unshift(false);
+      containDiv.classList.remove('unchecked');
+      containDiv.classList.add('checked');
     }
+    
   })
+
   
 }
 
-function createTodoOnCurrentPage(newText) {
-  const newDiv = document.createElement('div');
-  const newCheckbox = document.createElement('input');
-  const txtContainer = document.createElement('div');
-  
-  newCheckbox.setAttribute('type','checkbox');
-  newCheckbox.setAttribute('class','todo-checkmark');
-  const newP = document.createElement('p');
-  newP.textContent = newText;
-  txtContainer.appendChild(newP);
-  txtContainer.classList.add('txt-container');
-  
-  newDiv.setAttribute('class', 'checklist-item')
-  newDiv.appendChild(newCheckbox);
-  newDiv.appendChild(txtContainer)
-  
-  todoMain.prepend(newDiv);
-  createEventListenerForChecks(newCheckbox, newP)
 
+
+function createTodoAndDisplay(newText) {
+    const newDiv = document.createElement('div');
+    const newCheckbox = document.createElement('input');
+    const txtContainer = document.createElement('div');
+    
+    newCheckbox.setAttribute('type','checkbox');
+    newCheckbox.setAttribute('class','todo-checkmark');
+    const newP = document.createElement('p');
+    newP.textContent = newText;
+    txtContainer.appendChild(newP);
+    txtContainer.classList.add('txt-container');
+    
+    
+    newDiv.setAttribute('class', 'checklist-item');
+    newDiv.classList.add('unchecked');
+    newDiv.appendChild(newCheckbox);
+    newDiv.appendChild(txtContainer);
+    
+    todoMain.prepend(newDiv);
+
+}
+
+function reindexTodoIds() {
+
+    Array.from(todoMain.children).forEach((item, index) => {
+        item.setAttribute("id", `${index}`)
+        
+
+    })
 }
 
 function submitNewTodo () {
@@ -81,8 +212,9 @@ function submitNewTodo () {
         return
     }
     const newText = inputBox.value.trim();
-    createTodoOnCurrentPage(newText);
+    createTodoAndDisplay(newText);
     putTodoInCache(newText);
+    reindexTodoIds();
     inputBox.value = "";
     inputBox.focus();
     todoMain.scroll({top:0,behavior:'smooth'});
